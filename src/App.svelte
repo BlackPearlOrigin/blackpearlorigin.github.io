7102e9a390e05de0c5d022250f74c6de8f3a2734
Davi Santos <brisolo32@outlook.com>
2023-06-11 15:28:53 -0300

    Removed the footer and changed the bg

--- src/App.svelte
+++ src/App.svelte
@@ -1,101 +1,106 @@
 <script lang="ts">
-	import { 
-		Navbar, 
-		NavBrand, 
-		NavLi, 
-		NavUl, 
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
 		NavHamburger,
-		DarkMode, 
+		DarkMode,
 		Gallery,
-		GradientButton 
-	} from 'flowbite-svelte'
-	import { Footer, FooterCopyright, FooterLinkGroup, FooterLink, FooterBrand, FooterIcon } from 'flowbite-svelte'
-	
-	import { LogoApple, LogoDiscord, LogoGithub, LogoTux, LogoWindows } from "svelte-ionicons"
+		GradientButton,
+	} from 'flowbite-svelte';
+	import {
+		Footer,
+		FooterCopyright,
+		FooterLinkGroup,
+		FooterLink,
+		FooterBrand,
+		FooterIcon,
+	} from 'flowbite-svelte';
 
-	type OS = "Linux" | "Windows" | "Mac"
+	import {
+		LogoApple,
+		LogoDiscord,
+		LogoGithub,
+		LogoTux,
+		LogoWindows,
+	} from 'svelte-ionicons';
+
+	type OS = 'Linux' | 'Windows' | 'Mac';
 
 	const downBtn = (os: OS) => {
 		switch (os) {
-			case "Linux":
-				window.open("", "_newpage")
+			case 'Linux':
+				window.open('', '_newpage');
 				break;
-			case "Windows":
-				window.open("", "_newpage")
+			case 'Windows':
+				window.open('', '_newpage');
 				break;
-			case "Mac":
-				window.open("", "_newpage")
+			case 'Mac':
+				window.open('', '_newpage');
 				break;
 		}
-	}
+	};
 
-	const image = [
-		{ alt: "An image of BPO", src: "App-res.png"}
-	]
+	const image = [{ alt: 'An image of BPO', src: 'App-res.png' }];
 </script>
 
 <main class="relative px-8">
-	<Navbar 
+	<Navbar
 		navClass="navbar px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0 border-b"
-		let:hidden 
-		let:toggle>
+		let:hidden
+		let:toggle
+	>
 		<NavBrand href="/">
-		  <img
-			src="logo.svg"
-			class="dark:invert mr-3 h-6 sm:h-9"
-			alt="Flowbite Logo"
-		  />
+			<img
+				src="logo.svg"
+				class="dark:invert mr-3 h-6 sm:h-9"
+				alt="Flowbite Logo"
+			/>
 		</NavBrand>
-		<NavHamburger on:click={toggle} />
-		<NavUl {hidden}>
-		  <NavLi href="/" active={true}>Home</NavLi>
-		  <NavLi href="/about">About</NavLi>
-		  <NavLi href="/services">Services</NavLi>
-		  <NavLi href="/pricing">Pricing</NavLi>
-		  <NavLi href="/contact">Contact</NavLi>
-		</NavUl>
+		<NavHamburger on:click="{toggle}" />
+		<DarkMode />
+	</Navbar>
+	<div class="gradient-bg">
+		<div class="main-content">
+			<p class="text-6xl font-black text-center text-white">
+				<span class="underline decoration-sky-300">Simple</span> and
+				<span class="underline decoration-sky-300">Effortless</span> Library
+				Management
+			</p>
+			<p class="pt-3 text-xl font-medium text-center text-white">
+				Black Pearl Origin (BPO) is a FOSS library management app with a
+				built in search feature, plugin system and intuitive UI.
+			</p>
 
-		<DarkMode></DarkMode>
-	  </Navbar>
+			<div class="flex justify-center pt-[4rem]">
+				<div class="shadow-2xl ml-3 mr-3">
+					<GradientButton
+						on:click="{() => downBtn('Windows')}"
+						color="cyanToBlue"
+					>
+						<LogoWindows class="mt-1 mb-1" size="48px" />
+					</GradientButton>
+				</div>
+				<div class="shadow-2xl ml-3 mr-3">
+					<GradientButton
+						on:click="{() => downBtn('Mac')}"
+						color="cyanToBlue"
+					>
+						<LogoApple class="mt-1 mb-1" size="48px" />
+					</GradientButton>
+				</div>
+				<div class="shadow-2xl ml-3 mr-3">
+					<GradientButton
+						on:click="{() => downBtn('Linux')}"
+						color="cyanToBlue"
+					>
+						<LogoTux class="mt-1 mb-1" size="48px" />
+					</GradientButton>
+				</div>
+			</div>
 
-	<div class="main-content">
-		<p class="text-6xl font-black text-center text-white">
-			<span class="underline decoration-sky-300">Simple</span> and <span class="underline decoration-sky-300">Effortless</span> Library Management
-		</p>
-		<p class="pt-3 text-xl font-medium text-center text-white">Black Pearl Origin (BPO) is a FOSS library management app with a built in search feature, plugin system and intuitive UI.</p>
-		
-		<div class="flex justify-center pt-[4rem]">
-			<div class="shadow-2xl ml-3 mr-3">
-				<GradientButton on:click={() => downBtn("Windows")} color="cyanToBlue" >
-					<LogoWindows class="mt-1 mb-1"  size="48px"></LogoWindows>
-				</GradientButton>
-			</div>
-			<div class="shadow-2xl ml-3 mr-3">
-				<GradientButton on:click={() => downBtn("Mac")} color="cyanToBlue" >
-					<LogoApple class="mt-1 mb-1"  size="48px"></LogoApple>
-				</GradientButton>
-			</div>
-			<div class="shadow-2xl ml-3 mr-3">
-				<GradientButton on:click={() => downBtn("Linux")} color="cyanToBlue" >
-					<LogoTux class="mt-1 mb-1"  size="48px"></LogoTux>
-				</GradientButton>
-			</div>
+			<Gallery class="flex justify-center mt-[4rem]" items="{image}" />
 		</div>
-
-		<Gallery class="flex justify-center mt-[4rem]" items={image}></Gallery>
-	</div>
-	  
-	<div class="footer">
-		<Footer>
-			<FooterCopyright href="/" by="Black Pearl Origin" year={2023} />
-			<FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-				<FooterIcon href="https://github.com/BlackPearlOrigin">
-					<LogoGithub size="25px"></LogoGithub>
-				</FooterIcon>
-				<FooterIcon class="pl-5" href="https://discord.gg/vh3em37rqy">
-					<LogoDiscord size="25px"></LogoDiscord>
-				</FooterIcon>
-			</FooterLinkGroup>
-		</Footer>
 	</div>
 </main>
